42612d7fd50516820022f5fba95491b71fac4e8c
pipegoods <pipe.jaider@gmail.com>
2021-04-11 18:07:14 -0500

    jquery y materializeccs inicializados, con ejemplo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'dashboard';
+
+
+  ngOnInit() {
+    $('.dropdown-trigger').dropdown();
+  }
+
+
 }
